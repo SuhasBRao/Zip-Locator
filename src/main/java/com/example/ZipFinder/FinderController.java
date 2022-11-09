c7658206e2a8dc3da2872c80d02a3034816c24d5
@SuhasBRao@ <raos04567@gmail.com>
2022-11-09 12:36:19 +0530

    added contents from main application to controller

--- src/main/java/com/example/ZipFinder/FinderController.java
+++ src/main/java/com/example/ZipFinder/FinderController.java
@@ -2,11 +2,43 @@ package com.example.ZipFinder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class FinderController {
-    @GetMapping("/result")
-    public String
+
+    private String apiURL;
+
+    @GetMapping("/addZip")
+    public String addZip(Model model){
+        model.addAttribute("addZip", new ZipCode());
+        return "addZip";
+    }
+
+    @Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+    @PostMapping("/addZip")
+    public String showResult(@ModelAttribute ZipCode zipcode, Model model, RestTemplate restTemplate){
+        
+        model.addAttribute("addZip", zipcode);
+        apiURL = zipcode.getLink();
+
+        System.out.println(apiURL);
+        
+        ResultData resultdata = restTemplate.getForObject(
+					apiURL, ResultData.class);
+        model.addAttribute("ZipData", resultdata);
+
+        return "showResult";
+    }
+
 }
